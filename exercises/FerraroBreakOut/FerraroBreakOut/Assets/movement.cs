f04b9ad9bd5836d0f18fe05ea481a1e7c1237344
Pauline Ferraro <57684720+keywi@users.noreply.github.com>
2020-01-28 21:11:05 -0500

    submitting exercise 1
    ex1

---
+++ exercises/FerraroBreakOut/FerraroBreakOut/Assets/movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    void Start()
+    {
+        launchBall();
+    }
+
+    void launchBall()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        float forceX = Random.Range(-0.5f, 0.5f);
+
+        float forceY = -2f;
+
+        Vector3 force = new Vector3(forceX, forceY, 0);
+        float magnitude = 500;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        GetComponent<Rigidbody>().AddForce(force * magnitude);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BottomWall")
+        {
+            launchBall();
+        }
+    }
+}
+
+
+
+
+//need to add failure state when it hits the "BottomWall"
+//fix the movement issue
---
+++ exercises/FerraroBreakOut/FerraroBreakOut/Assets/paddlemovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddlemovement : MonoBehaviour
+{
+    public KeyCode leftKey;
+    public KeyCode RightKey;
+
+    public float paddleSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(leftKey) && transform.position.x > -8f)
+        {
+            transform.position = new Vector3(transform.position.x - paddleSpeed, transform.position.y, 0);
+        }
+        if (Input.GetKey(RightKey) && transform.position.x < 8f)
+        {
+            transform.position = new Vector3(transform.position.x + paddleSpeed, transform.position.y, 0);
+        }
+    }
+}
