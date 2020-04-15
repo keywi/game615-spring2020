5dc9d7020169d18e02961f3eb6219014ef8b9335
Pauline Ferraro <57684720+keywi@users.noreply.github.com>
2020-04-15 10:23:21 -0400

    upload assignment

---
+++ exercises/ARChess-master/Assets/_Scripts/SceneManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public string Choice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LoadBlackPlayer()
+    {
+        Choice = "black";
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void LoadWhitePlayer()
+    {
+        Choice = "white";
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+   
+}
+
